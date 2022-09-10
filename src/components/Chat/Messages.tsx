01525ba43756e211cc40d9fcd7d4fea97ca186e4
akal0 <abdallahkalenga1689@gmail.com>
2022-09-10 22:34:04 +0100

    Update Messages.tsx

--- src/components/Chat/Messages.tsx
+++ src/components/Chat/Messages.tsx
@@ -21,47 +21,30 @@ const Messages = () => {
 
   return (
     <div className="px-10 py-8 h-[calc(100vh-176px)] overflow-scroll">
-      {messages.length > 0 ? (
-        messages.map((message, index) => (
-          <Message message={message} key={index} />
-        ))
-      ) : user ? (
-        <div>
-          <div className={"flex gap-4"}>
-            <div className="flex flex-col items-center gap-1">
-              <BsFillChatLeftFill className="w-10 h-10 rounded-md text-slate-800" />
-              <span className="text-xs text-gray-500 font-bold mb-3 flex flex-col items-center">
-                Just now
-              </span>
-            </div>
+      {messages.length > 0
+        ? messages.map((message, index) => (
+            <Message message={message} key={index} />
+          ))
+        : user && (
+            <div>
+              <div className={"flex gap-4"}>
+                <div className="flex flex-col items-center gap-1">
+                  <BsFillChatLeftFill className="w-10 h-10 rounded-md text-slate-800" />
+                  <span className="text-xs text-gray-500 font-bold mb-3 flex flex-col items-center">
+                    Just now
+                  </span>
+                </div>
 
-            <div className="max-w-[80%] flex flex-col gap-8 content mt-16">
-              <p className="bg-slate-800 text-white px-6 py-4 rounded-tr-md rounded-br-md rounded-bl-md text-sm max-w-prose break-all">
-                You and this user haven't talked before! Start konnecting with{" "}
-                {user.displayName}!
-              </p>
+                <div className="max-w-[80%] flex flex-col gap-8 content mt-16">
+                  <p className="bg-slate-800 text-white px-6 py-4 rounded-tr-md rounded-br-md rounded-bl-md text-sm max-w-prose break-all">
+                    {user.displayName
+                      ? `You and ${user.displayName} haven't talked before, start konnecting with them!`
+                      : `Start konnecting with somebody! Search for users on the left to start!`}
+                  </p>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      ) : (
-        <div>
-          <div className={"flex gap-4"}>
-            <div className="flex flex-col items-center gap-1">
-              <BsFillChatLeftFill className="w-10 h-10 rounded-md text-slate-800" />
-              <span className="text-xs text-gray-500 font-bold mb-3 flex flex-col items-center">
-                Just now
-              </span>
-            </div>
-
-            <div className="max-w-[80%] flex flex-col gap-8 content mt-16">
-              <p className="bg-slate-800 text-white px-6 py-4 rounded-tr-md rounded-br-md rounded-bl-md text-sm max-w-prose break-all">
-                Yo! You haven't even konnected with anybody! Search for users on
-                the right to start konnecting!
-              </p>
-            </div>
-          </div>
-        </div>
-      )}
+          )}
     </div>
   );
 };
